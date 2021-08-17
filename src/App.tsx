97872308a0f5debe580f55c97355547bbca6151d
hardingcheng <hardingcheng@163.com>
2021-08-17 12:43:33 +0800

    ci: 修改配置文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./App.css";
-import { ProjectListScreen } from "./pages/project-list";
+import React from 'react'
+import './App.css'
+import { ProjectListScreen } from './pages/project-list'
 
 function App() {
   return (
     <div className="App">
       <ProjectListScreen />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
