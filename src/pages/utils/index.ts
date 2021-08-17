06f74405ebf4b028c671edba7e6441ff297dcec1
hardingcheng <hardingcheng@163.com>
2021-08-17 12:38:52 +0800

    ci: 修改配置文件

--- src/pages/utils/index.ts
+++ src/pages/utils/index.ts
@@ -1,57 +1,57 @@
 // 出入的参数都要进行类型定义
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export const isVoid = (value: unknown) =>
-  value === undefined || value === null || value === ''
+  value === undefined || value === null || value === "";
 
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: { [key: string]: unknown }) => {
   // 不操作原来的对象，自己的生成一个对象
-  const result = { ...object }
-  Object.keys(result).forEach(key => {
-    const value = result[key]
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
     if (isVoid(value)) {
-      delete result[key]
+      delete result[key];
     }
-  })
-  return result
-}
+  });
+  return result;
+};
 
 export const useMount = (callback: () => void) => {
   useEffect(() => {
-    callback()
+    callback();
     // TODO 依赖项里加上callback会造成无限循环，这个和useCallback以及useMemo有关系
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-}
+  }, []);
+};
 
 // 后面用泛型来规范类型
 export const useDebounce = <V>(value: V, delay?: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value)
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
-    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
     // 每次在上一个useEffect处理完以后再运行s
-    return () => clearTimeout(timeout)
-  }, [value, delay])
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
 
-  return debouncedValue
-}
+  return debouncedValue;
+};
 
 export const useArray = <T>(initialArray: T[]) => {
   // 不修改原来的值
-  const [value, setValue] = useState(initialArray)
+  const [value, setValue] = useState(initialArray);
   return {
     value,
     setValue,
     add: (item: T) => setValue([...value, item]),
     clear: () => setValue([]),
     removeIndex: (index: number) => {
-      const copy = [...value]
-      copy.splice(index, 1)
-      setValue(copy)
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
     },
-  }
-}
+  };
+};
