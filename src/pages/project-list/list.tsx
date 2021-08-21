a4f0bcbd108ac88b7110035a8224a8c807802086
hardingcheng <hardingcheng@163.com>
2021-08-22 07:58:48 +0800

    perf: React性能优化：React.memo

--- src/pages/project-list/list.tsx
+++ src/pages/project-list/list.tsx
@@ -17,8 +17,12 @@ interface ListProps extends TableProps<Project> {
   users: User[];
 }
 
-
-export const List = ( { users, ...props }: ListProps ) => {
+// React.memo 为高阶组件。默认情况下其只会对复杂对象做浅层对比.
+/*
+如果你的组件在相同 props 的情况下渲染相同的结果，那么你可以通过将其包装在 React.memo 中调用，以此通过记忆组件渲染结果的方式来提高组件的性能表现。这意味着在这种情况下，React 将跳过渲染组件的操作并直接复用最近一次渲染的结果。
+React.memo 仅检查 props 变更。如果函数组件被 React.memo 包裹，且其实现中拥有 useState，useReducer 或 useContext 的 Hook，当 state 或 context 发生变化时，它仍会重新渲染。
+ */
+export const List = React.memo( ( { users, ...props }: ListProps ) => {
   const { mutate } = useEditProject( useProjectsQueryKey() )
   const pinProject = ( id: number ) => ( pin: boolean ) => mutate( { id, pin } )
   return (
@@ -82,7 +86,7 @@ export const List = ( { users, ...props }: ListProps ) => {
       { ...props }
     />
   )
-}
+} )
 
 const More = ( { project }: { project: Project } ) => {
   const { startEditId } = useProjectModal()
